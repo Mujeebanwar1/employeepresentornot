4ce08e1b96cb6eb0f478d4e00ceef0b9afc80667
Mujeebanwar1 <mujeebanwar76@gmail.com>
2023-06-11 14:54:38 +0530

    presented

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace emoloyee_present_or_not
+{
+
+
+    class programm
+    {
+
+
+        static void Main(string[] args)
+
+        {
+            int IS_FULL_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
